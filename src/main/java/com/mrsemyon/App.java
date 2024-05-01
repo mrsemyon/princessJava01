5e9308396eb7669765ab229a2143cb739fb8e92f
Morozov Semyon <sky_net@mail.ru>
2024-05-02 01:34:12 +0500

    deleted playerNumber field from Game

--- src/main/java/com/mrsemyon/App.java
+++ src/main/java/com/mrsemyon/App.java
@@ -4,30 +4,32 @@ public class App{
     public void run(){
         Messenger.welcome();
 
+        Game game = new Game();
+        
+        int playerType = Messenger.insertPlayerType(1);
+
         Player firstPlayer = PlayerFactory.getPlayer(
-                (Messenger.insertPlayerType(Game.getPlayerNumber()) == 1)
+                (playerType == 1)
                         ? PlayerType.BOT : PlayerType.LEATHER,
-                Messenger.insertPlayerName(Game.getPlayerNumber())
+                Messenger.insertPlayerName(1)
         );
-        Game.incPlayerNumber();
+
+        playerType = Messenger.insertPlayerType(2);
 
         Player secondPlayer = PlayerFactory.getPlayer(
-                (Messenger.insertPlayerType(Game.getPlayerNumber()) == 1)
+                (playerType == 1)
                         ? PlayerType.BOT : PlayerType.LEATHER,
-                Messenger.insertPlayerName(Game.getPlayerNumber())
+                Messenger.insertPlayerName(2)
         );
-        Game.incPlayerNumber();
 
         Player[] players = new Player[] {firstPlayer, secondPlayer};
-
-
 
         firstPlayer.selectHand();
         secondPlayer.selectHand();
 
         Messenger.printSelectedHands(players);
 
-        Player winner = Game.play(players);
+        Player winner = game.play(players);
 
         Messenger.showWinner(winner);
 
