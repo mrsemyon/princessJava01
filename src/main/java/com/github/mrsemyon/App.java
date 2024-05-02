632abd1961cec0a8c4ee64b56a384b9c187f9e82
Morozov Semyon <sky_net@mail.ru>
2024-05-02 16:55:09 +0500

    added save to .json file functional

--- src/main/java/com/github/mrsemyon/App.java
+++ src/main/java/com/github/mrsemyon/App.java
@@ -1,13 +1,19 @@
 package com.github.mrsemyon;
 
+import com.fasterxml.jackson.databind.ObjectMapper;
+
+
+import java.io.File;
+import java.io.IOException;
 import java.util.List;
 import java.util.ArrayList;
 
 public class App{
-    public void run(){
+    public void run() throws IOException {
         Messenger messenger = new Messenger();
         Game game = new Game();
         List<Player> players = new ArrayList<>();
+        ObjectMapper mapper = new ObjectMapper();
 
         messenger.welcome();
         int playersCount = messenger.insertPlayersCount();
@@ -31,5 +37,6 @@ public class App{
         messenger.printSelectedHands(players);
         messenger.showWinner(game.play(players));
         messenger.printScores(players);
+        mapper.writeValue(new File("test.json"), players);
     }
 }
